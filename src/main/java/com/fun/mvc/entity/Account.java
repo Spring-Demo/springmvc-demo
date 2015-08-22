f859ceef69557cbb0fe81fe2b367424d6e199137
lian819 <lianxinzhong@126.com>
2015-08-22 16:02:05 +0800

    init the demo

---
+++ src/main/java/com/fun/mvc/entity/Account.java
@@ -0,0 +1,31 @@
+package com.fun.mvc.entity;
+
+public class Account {
+	private String cardNo;
+	private String password;
+	private float balance;
+
+	public String getCardNo() {
+		return cardNo;
+	}
+
+	public void setCardNo(String cardNo) {
+		this.cardNo = cardNo;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public float getBalance() {
+		return balance;
+	}
+
+	public void setBalance(float balance) {
+		this.balance = balance;
+	}
+}
---
+++ src/main/java/com/fun/mvc/web/controller/LoginController.java
@@ -0,0 +1,64 @@
+package com.fun.mvc.web.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import com.fun.mvc.entity.Account;
+
+
+
+public class LoginController extends AbstractController {
+	private String successView;
+	private String failView;
+
+	public String getSuccessView() {
+		return successView;
+	}
+
+	public void setSuccessView(String successView) {
+		this.successView = successView;
+	}
+
+	public String getFailView() {
+		return failView;
+	}
+
+	public void setFailView(String failView) {
+		this.failView = failView;
+	}
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String cardNo = request.getParameter("cardNo");
+		String password = request.getParameter("password");
+		Account account = getAccount(cardNo, password);
+
+		Map<String, Object> model = new HashMap<String, Object>();
+		if (account != null) {
+			model.put("account", account);
+			return new ModelAndView(getSuccessView(), model);
+		} else {
+			model.put("error", "卡号或密码不正确");
+			return new ModelAndView(getFailView(), model);
+		}
+	}
+
+	private Account getAccount(String cardNo, String password) {
+		if (cardNo.equals("123") && password.equals("123")) {
+			Account account = new Account();
+			account.setCardNo(cardNo);
+			account.setBalance(88.8F);
+			return account;
+		} else {
+			return null;
+		}
+	}
+	
+
+}
